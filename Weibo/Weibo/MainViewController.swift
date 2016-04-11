7d32fec2a7a4d904f22627fa2443c76a0cf3a606
hxbxiaobei <68059604@qq.com>
2016-04-11 18:46:57 +0800

    初始化项目 1.配置项目基本信息 2.创建项目的基本文件

---
+++ Weibo/Weibo/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabBarController.swift
+//  Weibo
+//
+//  Created by HuangXiaoBei on 16/4/11.
+//  Copyright © 2016年 huangxiaobei. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.orangeColor()
+    }
+
+    
+
+}
